faeafba1a877c9b4164a3772dd2530d59ef85981
Marcelo Suisse <ubriquejazz@gmail.com>
2022-09-24 20:35:03 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 1. Salida por pantalla
+ *
+ * 1. Escribe una programa que muestre tu nombre por pantalla.
+ *
+ * @author Luis Gonzalez
+ */
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Luis Gonzalez");
+        System.out.print("Presiona una tecla para continuar...");
+        String line =s.nextLine();
+    }
+}
